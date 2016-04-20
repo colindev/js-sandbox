0c1189c2c32eb5e9547fc0d7b7c970f4c8bfaf38
Esther <k1017739@gmail.com>
2016-04-20 11:05:34 +0800

    function

--- public/src/js/array.js
+++ public/src/js/array.js
@@ -24,6 +24,7 @@ function makePhrases() {
 // 4.3
 var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 var scores2nd = [77, 55, 51, 52, 44, 51];
+var costs = [.25, .27, .25, .25, .25, .25, .33, .31, .25, .29, .27, .22, .31, .25, .25, .33, .21, .25, .25, .25, .28, .25, .24, .22, .20, .25, .30, .25, .24, .25, .25, .25, .27, .25, .26, .29];
 
 function printAndGetHighScore(scores) {
   var output;
@@ -49,11 +50,27 @@ function getBestResults(scores, highScore){
   return bestSolutions;
 }
 
+function getCostMostEffectiveSolution(scores, costs,highScore) {
+  var cost = 100;
+  var index;
+  for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+      if (cost > costs[i]) {
+        index = i;
+        cost = cost[i];
+      }
+    }
+  }
+  return index;
+}
+
 var highScore = printAndGetHighScore(scores);
 console.log("Bubble tests: " + scores.length);
 console.log("Best score: " + highScore);
 var bestSolutions = getBestResults(scores, highScore);
 console.log("Solutions with the highest score: " + bestSolutions);
+var mostEffectiveSolution = getCostMostEffectiveSolution(scores, costs,highScore);
+console.log("Bubble Solution #" + mostEffectiveSolution + " is the most cost effective.")
 
 var highScore = printAndGetHighScore(scores2nd);
 console.log("Bubble tests: " + scores2nd.length);
