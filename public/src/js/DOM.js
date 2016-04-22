39ad64e95db215090ec82d472c954d13471d2c50
Esther <k1017739@gmail.com>
2016-04-22 10:25:11 +0800

    DOM

---
+++ public/src/js/DOM.js
@@ -0,0 +1,13 @@
+
+// Secret Code
+// var access = document.getElementById('code9');
+// var code = access.innerHTML;
+// code = code + " midnight";
+// alert(code);
+
+
+// change HTML content
+// set the element as var
+var planet = document.getElementById("greenplanet");
+// change HTML content
+planet.innerHTML = "Red Alert: hit by phaser fire!";
