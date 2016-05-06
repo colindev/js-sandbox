5351135ce71eceb919503c14092d5789a0f5bc5c
Esther <k1017739@gmail.com>
2016-05-06 15:06:57 +0800

    guess number update

--- public/src/js/guessNum.js
+++ public/src/js/guessNum.js
@@ -1,19 +1,26 @@
 
-var myNum = Math.floor(Math.random() * 10);
+var maxNum = 30;
+var myNum = Math.floor((Math.random() * maxNum) +1);
 var answer = false;
 var guessTimes = 0;
 var guessAns = [];
+var guessMaxTimes = 5;
 
 console.log(myNum);
 
+
 function checkGuess(guessNum) {
-  // first guess or hadn't been guessed
-  if (guessTimes == 0 || guessAns.indexOf(guessNum) == -1) {
-    alert("Sorry! You are wrong! ");
-    guessTimes++;
-  // check if the number had been guessed
-  } else if (guessAns.indexOf(guessNum) != -1) {
-    alert("Sorry! You've guessed this number'! ");
+  if ( guessNum > maxNum ||  guessNum < 1) {
+    alert("The number should be 1 ~ " + maxNum);
+  } else {
+    // first guess or hadn't been guessed
+    if (guessTimes == 0 || guessAns.indexOf(guessNum) == -1){
+      alert("Sorry! You are wrong!");
+      guessTimes++;
+    // check if the number had been guessed
+    } else if (guessAns.indexOf(guessNum) != -1) {
+      alert("Sorry! You've guessed this number'! ");
+    }
   }
   guessAns.push(guess);
   return;
@@ -29,15 +36,15 @@ function gameProcess(guess) {
   }
 }
 
-while (guessTimes <= 3) {
-  var guess = prompt("Please enter a number between 1 ~ 10.");
+while (guessTimes <= guessMaxTimes) {
+  var guess = prompt("Please enter a number between 1 ~ " + maxNum + ".");
   if (!gameProcess(guess)) {
     checkGuess(guess);
   } else {
     break;
   }
-  if (guessTimes == 3) {
-    alert("Wrong again! You've guessed 3 times! The number is " + myNum + "!");
+  if (guessTimes == guessMaxTimes) {
+    alert("You've guessed " + guessMaxTimes + " times! The number is " + myNum + "!");
     break;
   }
 }
